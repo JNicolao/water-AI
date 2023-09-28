9c12d82c07e1c9e9914a7b870ea6f36b53fcf3e7
JNicolao <bororio96@gmail.com>
2023-09-28 17:04:48 +0300

    Init files

---
+++ digital-card/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import "./assets/components/Info"
+import Info from "./assets/components/Info";
+
+export default function App() {
+  return (
+    <>
+      <Info />
+    </>
+  );
+}
---
+++ digital-card/src/assets/components/Info.jsx
@@ -0,0 +1,13 @@
+
+export default function Info(){
+    return (
+      <div className="info" style={{width: '100%', height: '100%', background: '#1A1B21'}}>
+        <img src="../images/jnic.jpeg"></img>
+        <div className="info-head">
+          <h1> Justine Nicolao</h1>
+          <h3>Frontend Developer</h3>
+          <h4>Website</h4>
+        </div>
+      </div>
+    );
+}
